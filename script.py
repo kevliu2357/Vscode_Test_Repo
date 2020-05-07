5e92e26d264a47eeb4a22ccfd714973542869eb0
Kev Liu <kevliu2357@gmail.com>
2020-05-07 22:19:35 +0800

    Removed Some Code

--- script.py
+++ script.py
@@ -1,21 +1,8 @@
-import math
-import sys
-from os import rename
 
 import numpy
 import requests
 
-# print(sys.version)
-print(sys.executable)  # commenting
-
 # print a
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 
